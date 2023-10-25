4c1e0fd23f0addb0643df7b6d87f1eb7b234454a
Delaney Sylvans <delaneysylvans@gmail.com>
2023-10-25 01:34:26 +0100

    timer with hardcoded value

--- quiz/main.go
+++ quiz/main.go
@@ -7,36 +7,93 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
+type Question struct {
+	question string
+	answer   string
+}
+
 func main() {
-
-	if file, err := os.Open("problems.csv"); err == nil {
-		defer file.Close()
-
-		csvReader := csv.NewReader(file)
-		if problems, err := csvReader.ReadAll(); err == nil {
-			var score int = 0
-			for _, problem := range problems {
-				reader := bufio.NewReader(os.Stdin)
-				fmt.Print(problem[0] + "= ")
-
-				text, _ := reader.ReadString('\n')
-				text = strings.Replace(text, "\n", "", -1)
-
-				if text == problem[1] {
-					score++
-				}
-			}
-
-			fmt.Println("Test Complete!")
-			fmt.Printf("\nScore: %d/%d", score, len(problems))
-		} else {
-			fmt.Println("Error reading problems")
-		}
-
-	} else {
+	file, err := os.Open("problems.csv")
+	if err != nil {
 		log.Fatal("Error while reading the file", err)
+		return
 	}
 
+	defer file.Close()
+
+	csvReader := csv.NewReader(file)
+	problems, err := csvReader.ReadAll()
+	if err != nil {
+		fmt.Println("Error reading problems")
+		return
+	}
+
+	var questions []Question
+	for _, question := range problems {
+		q := Question{}
+		q.question = question[0]
+		q.answer = question[1]
+		questions = append(questions, q)
+	}
+
+	score, err := askQuestions(questions, 2)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("\nTest Complete!")
+	fmt.Printf("\nScore: %d/%d", score, len(problems))
+
+}
+
+func askQuestions(questions []Question, limit int) (int, error) {
+	score := 0
+	timer := time.NewTimer(time.Duration(limit) * time.Second)
+	done := make(chan string)
+
+	go func() {
+		for {
+			reader := bufio.NewReader(os.Stdin)
+			text, err := reader.ReadString('\n')
+			if err != nil {
+				log.Fatal(err)
+			}
+			text = strings.Replace(text, "\n", "", -1)
+			done <- text
+		}
+	}()
+
+	for _, question := range questions {
+		answer, err := askQuestion(question, timer.C, done)
+		if err != nil && answer == -1 {
+			return score, nil
+		}
+		score += answer
+	}
+
+	return score, nil
+}
+
+func askQuestion(problem Question, timer <-chan time.Time, done <-chan string) (int, error) {
+	fmt.Printf("%s = ", problem.question)
+
+	for {
+		select {
+		case <-timer:
+			return -1, fmt.Errorf("Time limit exceeded")
+		case answer := <-done:
+			score := 0
+			if strings.Compare(problem.answer, answer) == 0 {
+				score = 1
+			} else {
+				return 0, fmt.Errorf("Wrong Answer")
+			}
+
+			return score, nil
+		}
+	}
 }
